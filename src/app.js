fee6baf2b165b8fd1a03accea31587628c32a2a5
MatiasGonzalezL <matias.alejandro.gonzalezl@gmail.com>
2022-01-11 23:15:03 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#mi-excusa").innerHTML = generateExcuse();
+};
+
+let generateExcuse = () => {
+  let su = ["Mi perro", "Mi gato", "Tu abuela", "Tu madre"];
+  let ver = ["se comió", "abrió", "molestó", "se tomó"];
+  let obj = ["mi tarea", "tu cuenta", "tu café", "su flor"];
+  let com = ["en la calle", "en la clase", "en el auto"];
+
+  let suInd = Math.floor(Math.random() * su.length);
+  let verInd = Math.floor(Math.random() * ver.length);
+  let objInd = Math.floor(Math.random() * obj.length);
+  let comInd = Math.floor(Math.random() * com.length);
+
+  return su[suInd] + " " + ver[verInd] + " " + obj[objInd] + " " + com[comInd];
 };
